448115b45a5e0382a706721bbc14f687d96a4c63
Jan Bárta <jenda@Jans-iMac.netis>
2020-08-17 11:27:10 +0200

    first commit

---
+++ HackerNews/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  HackerNews
+//
+//  Created by Jan Bárta on 17/07/2020.
+//  Copyright © 2020 Jan Bárta. All rights reserved.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
---
+++ HackerNews/Models/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  HackerNews
+//
+//  Created by Jan Bárta on 16/07/2020.
+//  Copyright © 2020 Jan Bárta. All rights reserved.
+//
+
+import Foundation
